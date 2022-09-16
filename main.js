c8470e1fce3b6e1cc7098eb60052b46576a84cb1
Nasly <naslicita30@hotmail.com>
2022-09-16 15:28:50 -0500

    Multiplication table

--- main.js
+++ main.js
@@ -13,8 +13,15 @@
 // }
 // sumar(1,2)
 /*ARROW FUNCTION*/
-const sumar=(a,b)=>{
-    let suma=a+b;
-    document.write(suma);
+// const sumar=(a,b)=>{
+//     let suma=a+b;
+//     document.write(suma);
+// }
+// sumar(1,2)
+
+
+let number=+prompt("Give me a number");
+for (let i = 1; i <= 9; i++) {
+    document.write(`${number}x${i}=${number*i}<br>`); 
+    
 }
-sumar(1,2)
