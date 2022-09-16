9daeef039c799206a9ef76abaaf52d43d7234987
Nasly <naslicita30@hotmail.com>
2022-09-16 15:11:30 -0500

    Tipos_de_Funciones

---
+++ main.js
@@ -0,0 +1,20 @@
+/*DECLARADA*/
+
+// function sumar(a,b){
+//     let suma=a+b;
+//     document.write(suma);
+// }
+// sumar(1,2)
+/*EXPRESADA*/
+
+// const sumar=function(a,b){
+//     let suma=a+b;
+//     document.write(suma);
+// }
+// sumar(1,2)
+/*ARROW FUNCTION*/
+const sumar=(a,b)=>{
+    let suma=a+b;
+    document.write(suma);
+}
+sumar(1,2)
